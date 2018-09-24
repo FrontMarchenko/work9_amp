ba365ed303713a94bd73ac04c14adc41a4a4328b
VolodymyrMarchenko <nokialeader@gmail.com>
2018-09-24 17:38:45 +0300

    amp

---
+++ gulp/inject.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var replace = require('gulp-replace');
+var fs = require('fs');
+gulp.task('inject', function(){
+
+ return gulp.src(['build/index.html'])
+    .pipe(replace('<link rel="stylesheet" href="css/app.css">', function(params) {
+      var css = fs.readFileSync('build/css/app.css', "utf8");
+
+      return '<style amp-custom>\n' + css + '\n</style>'
+    }))
+    .pipe(gulp.dest('build/'));
+});
+   
